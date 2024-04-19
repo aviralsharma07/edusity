1b99bdf31f938da2ec4395454624d99abd238605
aviralsharma07 <thisisavi200@gmail.com>
2024-04-19 09:05:42 +0530

    create about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./Components/About/About";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
@@ -11,6 +12,8 @@ const App = () => {
       <div className="container">
         <Title subTitle="OUR PROGRAM" title="What We Offer" />
         <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
       </div>
     </div>
   );
