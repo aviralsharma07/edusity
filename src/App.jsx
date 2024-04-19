0a0e041bd93825f5bbdd6293c8433dc489a45ba4
aviralsharma07 <thisisavi200@gmail.com>
2024-04-19 08:31:29 +0530

    create Programs and Title Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="OUR PROGRAM" title="What We Offer" />
+        <Programs />
+      </div>
     </div>
   );
 };
